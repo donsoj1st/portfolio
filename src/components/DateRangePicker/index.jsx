4a1b92edcc767408f1e37acf4cf2da034cc33cc6
Adesoji <donsoj1st@byui.edu>
2023-11-02 07:10:33 +0100

    commit

--- src/components/DateRangePicker/index.jsx
+++ src/components/DateRangePicker/index.jsx
@@ -16,9 +16,50 @@ const DateRangePicker = () => {
   const [secondInterval, setSecondInterval] = useState(null);
   const [firstCalender, setFirstCalender] = useState(getfirstCalender(0));
   const [secondCalender, setSecondCalender] = useState(getfirstCalender(1));
+
+  const firstDateSelected = (value) => {
+    console.log("first", value);
+    const currentDate = new Date(value.getFullYear(), value.getMonth());
+    const secondDate = new Date(
+      secondCalender.getFullYear(),
+      secondCalender.getMonth()
+    );
+    console.log("checking", currentDate.getTime() >= secondDate.getTime());
+    if (currentDate.getTime() > secondDate.getTime()) {
+      const newDate = new Date(value.getFullYear(), value.getMonth() + 1);
+      console.log("I am here", newDate);
+      setSecondCalender((prev) => {
+        prev = null;
+        return newDate;
+      });
+    }
+  };
+  const secondDateSelected = (value) => {
+    // console.log("second", value);
+  };
+  useEffect(() => {
+    console.log("range", firstInterval, "  ", secondInterval);
+  }, [firstInterval, secondInterval]);
+
+  const getDayclick = (value) => {
+    if (!firstInterval && !secondInterval) {
+      setFirstInterval(value);
+    } else if (firstInterval && !secondInterval) {
+      if (firstInterval > value) {
+        const holder = firstInterval;
+        setFirstInterval(value);
+        setSecondInterval(holder);
+      } else {
+        setSecondInterval(value);
+      }
+    } else {
+      setSecondInterval(null);
+      setFirstInterval(value);
+    }
+  };
   const getTodayInterval = () => {
-    setFirstInterval(getfirstCalender(0));
-    setSecondInterval(getfirstCalender(0));
+    setFirstInterval(firstCalender);
+    setSecondInterval(firstCalender);
   };
 
   const getYesterdayInterval = () => {
@@ -49,63 +90,16 @@ const DateRangePicker = () => {
     setFirstInterval(day);
     setSecondInterval(end);
   };
-  const firstDateSelected = (value) => {
-    console.log("first", value);
-    const currentDate = new Date(value.getFullYear(), value.getMonth());
-    const secondDate = new Date(
-      secondCalender.getFullYear(),
-      secondCalender.getMonth()
-    );
-    console.log("checking", currentDate.getTime() >= secondDate.getTime());
-    if (currentDate.getTime() > secondDate.getTime()) {
-      const newDate = new Date(value.getFullYear(), value.getMonth() + 1);
-      console.log("I am here", newDate);
-      setSecondCalender((prev) => {
-        prev = null;
-        return newDate;
-      });
-    }
-  };
-  const secondDateSelected = (value) => {
-    // console.log("second", value);
-  };
-  useEffect(() => {
-    console.log("range", firstInterval, "  ", secondInterval);
-  }, [firstInterval, secondInterval]);
-
-  const getDayclick = (value) => {
-    if (!firstInterval && !secondInterval) {
-      value;
-    } else if (firstInterval && !secondInterval) {
-      if (firstInterval > value) {
-        const holder = firstInterval;
-        value;
-        setSecondInterval(holder);
-      } else {
-        setSecondInterval(value);
-      }
-    } else {
-      setSecondInterval(null);
-      setFirstInterval(value);
-    }
-  };
 
   return (
     <>
       <input
-        name="rangePicker"
         style={{
           width: "650px",
           paddingLeft: "8px",
           paddingTop: "6px",
           paddingBottom: "6px",
           border: "2px solid #2684ff",
-        }}
-        onFocus={() => {
-          console.log("soji");
-        }}
-        onBlur={() => {
-          console.log("blure");
         }}
       />
       <div className="date-range">
